dfb53f7c1cd2cbf318da3c9fc16f72097edd719a
minecrc <minecrc411@gmail.com>
2020-04-25 08:51:40 +0700

    Helpppp

--- v2/Common/js/Dropdown.js
+++ v2/Common/js/Dropdown.js
@@ -1,90 +1,91 @@
 /* global $ */
 (() => {
-  const GAP = 24;
-  const MAX_MENU_HEIGHT = 300;
+    const GAP = 24;
+    const MAX_MENU_HEIGHT = 300;
 
-  function calculateMenuPosition(anchor, menu, { forceBottom } = {}) {
-    const topRestrictHeight = $("[data-dropdown-top-restrict='true']").height();
-    const bottomRestrictHeight =
-      $("[data-dropdown-bottom-restrict='true']").height() || 0;
+    function calculateMenuPosition(anchor, menu, { forceBottom } = {}) {
+        const topRestrictHeight = $("[data-dropdown-top-restrict='true']").height();
+        const bottomRestrictHeight =
+            $("[data-dropdown-bottom-restrict='true']").height() || 0;
 
-    const top = anchor.offset().top - $(window).scrollTop();
-    const bottom = top + anchor.outerHeight();
-    const windowHeight = window.innerHeight;
-    const bottomArea = windowHeight - bottom;
+        const top = anchor.offset().top - $(window).scrollTop();
+        const bottom = top + anchor.outerHeight();
+        const windowHeight = window.innerHeight;
+        const bottomArea = windowHeight - bottom;
 
-    if (bottomArea > 240 + bottomRestrictHeight || forceBottom) {
-      return {
-        top: bottom,
-        bottom: 'auto',
-        maxHeight: bottomArea - GAP - bottomRestrictHeight
-      };
+        if (bottomArea > 240 + bottomRestrictHeight || forceBottom) {
+            return {
+                top: bottom,
+                bottom: 'auto',
+                maxHeight: bottomArea - GAP - bottomRestrictHeight
+            };
+        }
+
+        return {
+            top: 'auto',
+            bottom: windowHeight - top,
+            maxHeight: top - GAP - topRestrictHeight
+        };
     }
 
-    return {
-      top: 'auto',
-      bottom: windowHeight - top,
-      maxHeight: top - GAP - topRestrictHeight
-    };
-  }
+    function placeDropdownMenu(anchor, menu) {
+        menu.css('width', anchor.outerWidth());
+        menu.css('max-width', anchor.outerWidth());
+        menu.css('left', anchor.offset().left);
 
-  function placeDropdownMenu(anchor, menu) {
-    menu.css('width', anchor.outerWidth());
-    menu.css('max-width', anchor.outerWidth());
-    menu.css('left', anchor.offset().left);
+        const { top, bottom, maxHeight } = calculateMenuPosition(anchor, menu);
 
-    const { top, bottom, maxHeight } = calculateMenuPosition(anchor, menu);
+        menu.css('top', top);
+        menu.css('bottom', bottom);
+        menu.css('max-height', Math.min(maxHeight, MAX_MENU_HEIGHT));
+    }
 
-    menu.css('top', top);
-    menu.css('bottom', bottom);
-    menu.css('max-height', Math.min(maxHeight, MAX_MENU_HEIGHT));
-  }
+    function initDropdown() {
+        const dropdown = $(this);
 
-  function initDropdown() {
-    const dropdown = $(this);
+        if (dropdown.attr('data-init')) return;
+        dropdown.attr('data-init', true);
 
-    if (dropdown.attr('data-init')) return;
-    dropdown.attr('data-init', true);
+        const button = dropdown.find('.mz-dropdown__button');
+        const buttonText = button.find('.mz-dropdown__button-text');
+        const options = dropdown.find('li.mz-dropdown__option');
+        const dropdownMenu = dropdown.find('.mz-dropdown__menu');
 
-    const button = dropdown.find('.mz-dropdown__button');
-    const buttonText = button.find('.mz-dropdown__button-text');
-    const options = dropdown.find('li.mz-dropdown__option');
-    const dropdownMenu = dropdown.find('.mz-dropdown__menu');
+        dropdown.on('shown.bs.dropdown', () => {
+            placeDropdownMenu(button, dropdownMenu);
+        });
 
-    dropdown.on('shown.bs.dropdown', () => {
-      placeDropdownMenu(button, dropdownMenu);
-    });
+        $(window).resize(() => {
+            placeDropdownMenu(button, dropdownMenu);
+        });
 
-    $(window).resize(() => {
-      placeDropdownMenu(button, dropdownMenu);
-    });
+        document.addEventListener('scroll', () => {
+            dropdown.removeClass('open');
+        });
 
-    document.addEventListener('scroll', () => {
-      dropdown.removeClass('open');
-    });
+        options.on('click', e => {
+            e.preventDefault();
 
-    options.on('click', e => {
-      e.preventDefault();
+            buttonText.html($(e.target).html());
+            buttonText.addClass('--selected');
 
-      buttonText.html($(e.target).html());
-      buttonText.addClass('--selected');
+            const option = $(e.target);
 
-      const option = $(e.target);
+            options.removeClass('--active');
+            option.addClass('--active');
+            option
+                .parent()
+                .siblings('button.mz-dropdown__button')
+                .trigger('change', [option.text(), option.data('value')]);
+        });
+    }
 
-      options.removeClass('--active');
-      option.addClass('--active');
-      option
-        .parent()
-        .siblings('button.mz-dropdown__button')
-        .trigger('change', [option.text(), option.data('value')]);
-    });
-  }
+    const dropdowns = $('.mz-dropdown');
+    console.log(dropdowns);
+    dropdowns.each(initDropdown);
 
-  const dropdowns = $('.mz-dropdown');
-  dropdowns.each(initDropdown);
-
-  // Expose `initDropdown` to $ for reuses
-  $.initDropdown = initDropdown;
-  $.calculateMenuPosition = calculateMenuPosition;
-  $.placeDropdownMenu = placeDropdownMenu;
+    // Expose `initDropdown` to $ for reuses
+    $.initDropdown = initDropdown;
+    $.calculateMenuPosition = calculateMenuPosition;
+    $.placeDropdownMenu = placeDropdownMenu;
 })();
--- v2/Common/js/DropdownInput.js
+++ v2/Common/js/DropdownInput.js
@@ -1,13 +1,14 @@
 /* global $ */
 (() => {
-  const dropdown = $('.mz-dropdown');
+    const dropdown = $('.mz-dropdown');
+    console.log(dropdown);
+    dropdown.on('change', (e, label, value) => {
+        const targetId = e.target.id;
+        console.log(targetId);
+        const input = $(e.target)
+            .parent()
+            .siblings(`#${targetId.replace('-dropdown', '')}`);
 
-  dropdown.on('change', (e, label, value) => {
-    const targetId = e.target.id;
-    const input = $(e.target)
-      .parent()
-      .siblings(`#${targetId.replace('-dropdown', '')}`);
-
-    input.attr('value', value);
-  });
+        input.attr('value', value);
+    });
 })();
