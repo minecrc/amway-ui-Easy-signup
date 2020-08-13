f921311c900290efe6648a5a481de7a098c8dee3
minecrc <30970264+minecrc@users.noreply.github.com>
2020-08-13 15:16:41 +0700

    Update Field for Name Surname
    Add Name and SurAme Field for First Name Eng-language
    and Sur-Name Language

--- v2/Common/js/Input.js
+++ v2/Common/js/Input.js
@@ -1,24 +1,24 @@
 /* global $ */
 (() => {
-  function initInput() {
-    const input = $(this).find('.mz-input__input');
-    const clearButton = $(this).find('.mz-input__clear-button');
+    function initInput() {
+        const input = $(this).find('.mz-input__input');
+        const clearButton = $(this).find('.mz-input__clear-button');
 
-    input.on('change keyup paste', () => {
-      if (input.val().trim()) {
-        clearButton.addClass('--active');
-      } else {
-        clearButton.removeClass('--active');
-      }
-    });
+        input.on('change keyup paste', () => {
+            if (input.val().trim()) {
+                clearButton.addClass('--active');
+            } else {
+                clearButton.removeClass('--active');
+            }
+        });
 
-    clearButton.on('click', () => {
-      input.val('');
-      input.trigger('change');
-    });
-  }
+        clearButton.on('click', () => {
+            input.val('');
+            input.trigger('change');
+        });
+    }
 
-  const inputs = $('.mz-input');
+    const inputs = $('.mz-input');
 
-  inputs.each(initInput);
+    inputs.each(initInput);
 })();
